f638fd77f822bead41f5618dba4531c07f3cf6ad
DEFLIK <52451655+DEFLIK@users.noreply.github.com>
2022-03-08 00:00:50 +0500

    dao & tests proj deploy

---
+++ dao-core/DataManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data;
+using Npgsql;
+
+namespace dao_core
+{
+    class DataManager
+    {
+        private static void Main(string[] args)
+        {
+        }
+
+        private static bool TestConnection()
+        {
+            using var connection = GetConnection();
+            connection.Open();
+            return connection.State != ConnectionState.Open;
+        }
+
+        private static NpgsqlConnection GetConnection() =>
+            new NpgsqlConnection(@"Server=localhost;Port=5432;User Id=postgres;Password=none;Database=RMM;");
+    }
+}
