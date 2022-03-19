4a481c2ca9c6b6bd64d84eda434aecec3fc9cf59
DEFLIK <52451655+DEFLIK@users.noreply.github.com>
2022-03-19 13:04:29 +0500

    auth & device-panel modules deploy

---
+++ web-core/ClientApp/src/app/auth-module/auth.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './components/login/login.component';
+import { RegComponent } from './components/reg/reg.component';
+
+
+
+@NgModule({
+  declarations: [LoginComponent, RegComponent],
+  imports: [
+    CommonModule
+  ], 
+  exports: [
+    LoginComponent, RegComponent
+  ]
+})
+export class AuthModule { }
