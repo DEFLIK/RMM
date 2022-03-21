2c647d8df56eba21863ba562c9cbf2a316978546
DEFLIK <52451655+DEFLIK@users.noreply.github.com>
2022-03-21 17:58:23 +0500

    session service

---
+++ web-core/ClientApp/src/app/auth-module/services/cacheService/session.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class SessionService {
+    private get currSessionId(): string {
+        return localStorage.getItem('selectedSessionId');
+    }
+
+    private set currSessionId(value: string) {
+        localStorage.setItem('selectedSessionId', value);
+    }
+
+    constructor() {}
+
+    public cacheSession(sessionId: string, hash: string): void {
+        localStorage.setItem(sessionId, hash);
+    }
+
+    public setCurrentSession(sessionId: string): void {
+        this.currSessionId = sessionId;
+    }
+
+    public getCurrentSessionHash(): string {
+        if (this.currSessionId === '') {
+            throw new Error(`Failed to get session hash: there is no binded sessions`);
+        }
+
+        return localStorage.getItem(this.currSessionId);
+    }
+
+    public getCurrentSessionId(): string {
+        return this.currSessionId;
+    }
+
+    public removeSession(sessionId: string): void {
+        if (localStorage.getItem(sessionId) === null) {
+            throw new Error(`Failed to remove session ${sessionId}, there is no such`);
+        }
+
+        localStorage.removeItem(sessionId);
+        if (this.currSessionId === sessionId) {
+            this.currSessionId = '';
+        }
+    }
+}
--- web-core/ClientApp/src/app/auth-module/services/encrypt/encryption.service.ts
+++ web-core/ClientApp/src/app/auth-module/services/encrypt/encryption.service.ts
@@ -9,10 +9,10 @@ export class EncryptionService {
     }
 
     public async encryptStringAsync(str: string): Promise<string> {
-        const msgUint8: Uint8Array = new TextEncoder().encode(str);                           // encode as (utf-8) Uint8Array
-        const hashBuffer: ArrayBuffer = await crypto.subtle.digest('SHA-256', msgUint8);           // hash the message
-        const hashArray: number[] = Array.from(new Uint8Array(hashBuffer));                     // convert buffer to byte array
-        const hashHex: string = hashArray.map((b: number) => b.toString(16).padStart(2, '0')).join(''); // convert bytes to hex string
+        const msgUint8: Uint8Array = new TextEncoder().encode(str);
+        const hashBuffer: ArrayBuffer = await crypto.subtle.digest('SHA-256', msgUint8);
+        const hashArray: number[] = Array.from(new Uint8Array(hashBuffer));
+        const hashHex: string = hashArray.map((b: number) => b.toString(16).padStart(2, '0')).join('');
 
         return hashHex;
     }
