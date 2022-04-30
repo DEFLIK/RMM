df4170b45b12f50b4cee47e0d6bf4b8b0828e86e
DEFLIK <52451655+DEFLIK@users.noreply.github.com>
2022-04-30 14:27:23 +0500

    enabled postgres timestamp legacy behavior

--- Rmm.Domain/Rmm.Domain.Core/Startup.cs
+++ Rmm.Domain/Rmm.Domain.Core/Startup.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
@@ -80,6 +81,8 @@ namespace Rmm.Domain.Core
             app.UseRouting();
 
             app.UseAuthorization();
+
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 
             app.UseEndpoints(endpoints =>
             {
