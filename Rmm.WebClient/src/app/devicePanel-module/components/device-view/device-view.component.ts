6876b981f24db61462b3cd4af4e4fd4e079a1b8c
DEFLIK <52451655+DEFLIK@users.noreply.github.com>
2022-04-30 14:28:46 +0500

    renamed to view + removing unused methods

---
+++ Rmm.WebClient/src/app/devicePanel-module/components/device-view/device-view.component.ts
@@ -0,0 +1,35 @@
+import { Component, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { ChartConfiguration, ChartData, ChartDataset, ChartOptions, ChartType } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
+import { interval, Subscription } from 'rxjs';
+import { DeviceStaticInfo } from '../../models/deviceInfo';
+import { DeviceState } from '../../models/deviceState';
+import { DeviceSystemLogs } from '../../models/deviceSystemLogs';
+import { DeviceScreenService } from '../../services/deviceScreen/device-screen.service';
+import { DevicesStorageService } from '../../services/deviceStorage/devices-storage.service';
+
+@Component({
+    selector: 'app-device-view',
+    templateUrl: './device-view.component.html',
+    styleUrls: ['./device-view.component.less']
+})
+export class DeviceViewComponent implements OnInit, OnDestroy {
+    public imageToShow?: string | ArrayBuffer | null;
+    public selectedDevice!: DeviceStaticInfo;
+    private _deviceUpdater!: Subscription;
+    
+    constructor(private _storage: DevicesStorageService, private _screen: DeviceScreenService) { }
+    public ngOnInit(): void {
+
+        this._deviceUpdater = this._storage
+            .onDeviceSelected$
+            .subscribe((device: DeviceStaticInfo) => {
+                this.selectedDevice = device;
+            });
+    }
+
+    public ngOnDestroy(): void {
+        this._deviceUpdater.unsubscribe();
+    }
+}
+
