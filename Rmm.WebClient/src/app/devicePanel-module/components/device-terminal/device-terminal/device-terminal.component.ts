a7b6d10b29821457c3ae86c10a17f284df920fc8
DEFLIK <52451655+DEFLIK@users.noreply.github.com>
2022-05-03 23:11:36 +0500

    terminal logic upd

--- Rmm.WebClient/src/app/devicePanel-module/components/device-terminal/device-terminal/device-terminal.component.ts
+++ Rmm.WebClient/src/app/devicePanel-module/components/device-terminal/device-terminal/device-terminal.component.ts
@@ -1,23 +1,62 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { interval } from 'rxjs';
+import { Component, HostListener, Input, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { interval, Subscription } from 'rxjs';
+import { DeviceStaticInfo } from 'src/app/devicePanel-module/models/deviceInfo';
 import { DeviceSystemLogs } from 'src/app/devicePanel-module/models/deviceSystemLogs';
 import { DevicesStorageService } from 'src/app/devicePanel-module/services/deviceStorage/devices-storage.service';
+import { DeviceManipulationService } from 'src/app/devicePanel-module/services/deviceTerminal/device-manipulation.service';
 
 @Component({
     selector: 'app-device-terminal',
     templateUrl: './device-terminal.component.html',
     styleUrls: ['./device-terminal.component.less']
 })
-export class DeviceTerminalComponent implements OnInit {
+export class DeviceTerminalComponent implements OnInit, OnDestroy {
     public selectedDeviceLogs?: DeviceSystemLogs;
     public terminalLogs: string[] = [];
+    public inputForm: FormGroup = new FormGroup({
+        input: new FormControl('')
+    });
+    private _selectedDeviceId!: string | null;
+    private _logsUpdater!: Subscription;
+    private _idUpdater!: Subscription;
 
-    constructor(private _storage: DevicesStorageService) { }
+    constructor(
+        private _storage: DevicesStorageService,
+        private _manipulation: DeviceManipulationService
+    ) { }
+
     public ngOnInit(): void { 
-        this._storage
+        this._selectedDeviceId = this._storage.selectedDeviceId;
+
+        this._logsUpdater = this._storage
             .onSelectedLogsRefresh$
             .subscribe((logs: DeviceSystemLogs) => {
                 this.terminalLogs = logs.terminalLog ?? ['No logs found...'];
             });
+
+        this._idUpdater = this._storage
+            .onDeviceSelected$
+            .subscribe((device: DeviceStaticInfo) => {
+                this._selectedDeviceId = device.id;
+            });
+    }
+
+    public ngOnDestroy(): void {
+        this._idUpdater.unsubscribe();
+        this._logsUpdater.unsubscribe();
+    }
+
+    public executeCommand(): void {
+        const inputCommand: string = this.inputForm.get('input')?.value;
+        console.log(inputCommand);
+
+        if (this._selectedDeviceId && inputCommand) {
+            this._manipulation
+                .execute(this._selectedDeviceId, inputCommand)
+                .subscribe((logs: string[]) => {
+                    this.terminalLogs = logs;
+                }); 
+        }
     }
 }
