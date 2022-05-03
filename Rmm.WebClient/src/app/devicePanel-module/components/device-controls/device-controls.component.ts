9cfdabb76e5328c4b220f12e596a9a230003c91a
DEFLIK <52451655+DEFLIK@users.noreply.github.com>
2022-05-03 23:10:24 +0500

    device controls component

--- Rmm.WebClient/src/app/devicePanel-module/components/device-controls/device-controls.component.ts
+++ Rmm.WebClient/src/app/devicePanel-module/components/device-controls/device-controls.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { DeviceStaticInfo } from '../../models/deviceInfo';
+import { DevicesStorageService } from '../../services/deviceStorage/devices-storage.service';
 
 @Component({
-  selector: 'app-device-controls',
-  templateUrl: './device-controls.component.html',
-  styleUrls: ['./device-controls.component.less']
+    selector: 'app-device-controls',
+    templateUrl: './device-controls.component.html',
+    styleUrls: ['./device-controls.component.less']
 })
-export class DeviceControlsComponent implements OnInit {
+export class DeviceControlsComponent {
+    @Input()
+    public selectedDevice!: DeviceStaticInfo;
 
-  constructor() { }
+    constructor() { }
+    // public ngOnDestroy(): void {
+    //     // this._deviceUpdater.unsubscribe();
+    // }
 
-  ngOnInit(): void {
-  }
+    // public ngOnInit(): void {
+    //     // this._storage.
+
+    //     // this._deviceUpdater = this._storage
+    //     //     .onDeviceSelected$
+    //     //     .subscribe((info: DeviceStaticInfo) => {
+    //     //         this.selectedDevice = info;
+    //     //     });
+    // }
 
 }
