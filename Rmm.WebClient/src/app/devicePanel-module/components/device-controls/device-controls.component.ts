b766c220cbf0045759a38ecbd14b063ea47f46c1
DEFLIK <52451655+DEFLIK@users.noreply.github.com>
2022-05-11 00:54:52 +0500

    controls logic

--- Rmm.WebClient/src/app/devicePanel-module/components/device-controls/device-controls.component.ts
+++ Rmm.WebClient/src/app/devicePanel-module/components/device-controls/device-controls.component.ts
@@ -2,29 +2,44 @@ import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { DeviceStaticInfo } from '../../models/deviceInfo';
 import { DevicesStorageService } from '../../services/deviceStorage/devices-storage.service';
+import { DeviceManipulationService } from '../../services/deviceTerminal/device-manipulation.service';
 
 @Component({
     selector: 'app-device-controls',
     templateUrl: './device-controls.component.html',
     styleUrls: ['./device-controls.component.less']
 })
-export class DeviceControlsComponent {
+export class DeviceControlsComponent implements OnInit, OnDestroy {
     @Input()
     public selectedDevice!: DeviceStaticInfo;
+    private _deviceUpdater!: Subscription;
 
-    constructor() { }
-    // public ngOnDestroy(): void {
-    //     // this._deviceUpdater.unsubscribe();
-    // }
+    constructor(
+        private _man: DeviceManipulationService,
+        private _storage: DevicesStorageService
+    ) { }
 
-    // public ngOnInit(): void {
-    //     // this._storage.
+    public ngOnDestroy(): void {
+        this._deviceUpdater.unsubscribe();
+    }
 
-    //     // this._deviceUpdater = this._storage
-    //     //     .onDeviceSelected$
-    //     //     .subscribe((info: DeviceStaticInfo) => {
-    //     //         this.selectedDevice = info;
-    //     //     });
-    // }
+    public ngOnInit(): void {
+        this._storage.onDeviceSelected$.subscribe((device: DeviceStaticInfo) => {
+            this.selectedDevice = device;
+        });
+
+        this._deviceUpdater = this._storage
+            .onDeviceSelected$
+            .subscribe((info: DeviceStaticInfo) => {
+                this.selectedDevice = info;
+            });
+    }
+
+    public execute(com: string): void {
+        console.log(this.selectedDevice);
+        if (this.selectedDevice) {
+            this._man.execute(this.selectedDevice.id, com).subscribe();
+        }
+    }
 
 }
