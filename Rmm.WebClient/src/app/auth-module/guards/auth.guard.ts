9bab9880dcc0fe763602246d4ee7df4ed1925410
DEFLIK <52451655+DEFLIK@users.noreply.github.com>
2022-05-17 14:18:51 +0500

    auth guard upd

--- Rmm.WebClient/src/app/auth-module/guards/auth.guard.ts
+++ Rmm.WebClient/src/app/auth-module/guards/auth.guard.ts
@@ -1,6 +1,7 @@
+import { HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, CanLoad, Route, Router, RouterModule, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 import { AuthService } from '../services/auth/auth.service';
 
 @Injectable({
@@ -13,33 +14,40 @@ export class AuthGuard implements CanActivate, CanLoad, CanActivateChild {
         private _router: Router
     ) {}
 
-    public async canActivate(
+    public canActivate(
         route: ActivatedRouteSnapshot,
-        state: RouterStateSnapshot): Promise<boolean> {
-        if (!this._auth.isAuthorized) {
-            await this._router.navigateByUrl('');
-        }
-
-        return this._auth.isAuthorized;
+        state: RouterStateSnapshot): Observable<boolean> {
+        return this.validate(this._router.url);
     }
 
-    public async canLoad(
+    public canLoad(
         route: Route, 
-        segments: UrlSegment[]): Promise<boolean> {
-        if (!this._auth.isAuthorized) {
-            await this._router.navigateByUrl('');
-        }
-
-        return this._auth.isAuthorized;
+        segments: UrlSegment[]): Observable<boolean> {
+        return this.validate(this._router.url);
     }
 
-    public async canActivateChild(
+    public canActivateChild(
         childRoute: ActivatedRouteSnapshot, 
-        state: RouterStateSnapshot): Promise<boolean> {
-        if (!this._auth.isAuthorized) {
-            await this._router.navigateByUrl('');
-        }
-
-        return this._auth.isAuthorized;
+        state: RouterStateSnapshot): Observable<boolean> {
+        return this.validate(this._router.url);
     }
+
+    public validate(route: string): Observable<boolean> {
+        return this._auth.isCurrentSessionOpen()
+            .pipe(
+                catchError(() => {
+                    this._router.navigateByUrl('/auth/login');
+
+                    return of();
+                }),
+                map((resp: HttpResponse<void>) => {
+                    if (!resp.ok) {
+                        this._router.navigateByUrl('/auth/login');
+                    }
+
+                    return resp.ok;
+                })
+            );
+    }
+
 }
