ec932df8c5606972725f91b0c634b7b48c39e0d5
DEFLIK <52451655+DEFLIK@users.noreply.github.com>
2022-05-17 14:20:14 +0500

    navigation interceptor for module loading display

--- Rmm.WebClient/src/app/app.component.ts
+++ Rmm.WebClient/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router, RouterEvent } from '@angular/router';
 
 @Component({
     selector: 'app-root',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.less']
 })
 export class AppComponent {
+    public loading: boolean = false;
     private _title : string = 'app';
+    
+
+    constructor(private _router: Router) {
+        this._router
+            .events
+            .subscribe((e: any) => {
+                this.navigationInterceptor(e);
+            });
+    }
+
+    public navigationInterceptor(event: RouterEvent): void {
+        if (event instanceof NavigationStart) {
+            this.loading = true;
+        }
+        if (event instanceof NavigationEnd) {
+            this.loading = false;
+        }
+
+        // Set loading state to false in both of the below events to hide the spinner in case a request fails
+        if (event instanceof NavigationCancel) {
+            this.loading = false;
+        }
+        if (event instanceof NavigationError) {
+            this.loading = false;
+        }
+    }
 }
