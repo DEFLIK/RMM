a052e5254bd1d16003dbac30a50e0937209c2d39
DEFLIK <52451655+DEFLIK@users.noreply.github.com>
2022-05-23 15:28:52 +0500

    settings component relocation

---
+++ Rmm.WebClient/src/app/devicePanel-module/components/settings/settings.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-settings',
+    templateUrl: './settings.component.html',
+    styleUrls: ['./settings.component.less']
+})
+export class SettingsComponent implements OnInit {
+
+    constructor() { }
+
+    public ngOnInit(): void {
+        throw Error('');
+    }
+
+}
